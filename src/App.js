459ad105818c24275b401b7cb9a11be03c57b008
isha <IshaGajendrasingh.Thakur@student.ufv.ca>
2024-02-01 11:04:16 -0800

    modified app.js

--- src/App.js
+++ src/App.js
@@ -3,6 +3,9 @@ import './App.css';
 import {BrowserRouter, Routes, Route} from 'react-router-dom';
 import HomePage from './Pages/HomePage/HomePage';
 import AddRecipe from './Pages/AddRecipe/AddRecipe';
+import SelectedRecipe from './Components/SelectedRecipe/SelectedRecipe';
+import RegisterUser from './Pages/RegisterUser/RegisterUser';
+import LoginUser from './Pages/LoginUser/LoginUser';
 
 function App() {
   return (
@@ -11,6 +14,9 @@ function App() {
         <Routes>
           <Route path="/" element={<HomePage />} />
           <Route path="/recipe/add" element={<AddRecipe />} />
+          <Route path="/recipes/:id" element={<SelectedRecipe/>} />
+          <Route path="/users/register" element={<RegisterUser />} />
+          <Route path="/users/login" element={<LoginUser />} />
         </Routes>
       </BrowserRouter>
     </div>
